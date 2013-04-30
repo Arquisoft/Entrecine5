40e571ec0752de954d61d5dd20a0ee3141412443
Aitor Baragaño Fernández <aitorbf@gmail.com>
2013-05-01 01:28:47 +0200

    Funcionalidades para el administrador en play: ver sesiones y borrar sesiones.

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,11 +1,14 @@
 package controllers;
 
 import models.Pelicula;
+import models.Sesion;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.admin.anadir;
 import views.html.admin.index;
+import views.html.admin.sesiones;
+import views.html.admin.anadirSesion;
 
 public class Admin extends Controller {
 
@@ -36,7 +39,36 @@ public class Admin extends Controller {
 
 		return redirect(routes.Cliente.index());
 	}
+	
+	public static Result verSesiones() {
+		return ok(sesiones.render(Sesion.all(), sesionForm));
+	}
+	
+	public static Result nuevaSesion() {
+		return ok(anadirSesion.render(sesionForm));
+	}
+	
+	public static Result crearSesion() {
 
+		Form<Sesion> formularioCumplimentado = sesionForm.bindFromRequest();
+
+		if (formularioCumplimentado.hasErrors()) {
+			return badRequest(sesiones.render(Sesion.all(), sesionForm));
+		}
+
+		Sesion.create(formularioCumplimentado.get());
+		return redirect(routes.Admin.verSesiones());
+
+	}
+	
+	public static Result eliminarSesion(Long id) {
+
+		Sesion.remove(id);
+
+		return redirect(routes.Admin.verSesiones());
+	}
+	
 	static Form<Pelicula> peliculaForm = Form.form(Pelicula.class);
+	static Form<Sesion> sesionForm = Form.form(Sesion.class);
 
 }
