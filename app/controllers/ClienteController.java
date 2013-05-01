faeb9728ab57920262f61a2308c697c27978f5c5
BeLi <belipowa@gmail.com>
2013-05-01 03:58:14 +0200

    Implementada la vista fichaPelicula

--- app/controllers/ClienteController.java
+++ app/controllers/ClienteController.java
@@ -1,19 +1,27 @@
 package controllers;
 
 import models.Pelicula;
-import models.Usuario;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.cliente.comprar;
 import views.html.cliente.index;
+import views.html.cliente.fichaPelicula;
 
 public class ClienteController extends Controller {
 	
 	
 
 	public static Result index() {
-		return ok(index.render(Pelicula.all(), peliculaForm, isSesionIniciada()));
+		return ok(index.render(Pelicula.all(), peliculaForm, UsuarioController.isSesionIniciada()));
+	}
+	
+	public static Result verFichaPelicula(Long id){
+		
+		Pelicula pelicula = Pelicula.find.ref(id);
+
+		return ok(fichaPelicula.render(pelicula));
+		
 	}
 
 	public static Result comprarEntrada(Long id) {
@@ -21,23 +29,6 @@ public class ClienteController extends Controller {
 		Pelicula pelicula = Pelicula.find.ref(id);
 
 		return ok(comprar.render(pelicula));
-	}
-	
-	private static Usuario isSesionIniciada(){
-		
-		Usuario usuario = null;
-		String usuarioId = session("usuarioId");
-		
-		if (usuarioId != null) {
-			usuario = Usuario.find.ref(Long.parseLong(usuarioId));
-			
-			if (usuario != null){
-				session("usuarioId", usuarioId);
-			}
-		}
-		
-		return usuario;
-			
 	}
 
 	static Form<Pelicula> peliculaForm = Form.form(Pelicula.class);
--- app/controllers/UsuarioController.java
+++ app/controllers/UsuarioController.java
@@ -1,17 +1,35 @@
 package controllers;
 
+import models.Usuario;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.usuario.registro;
 
-public class UsuarioController extends Controller{
-	
-	public static Result registroNuevo(){
+public class UsuarioController extends Controller {
+
+	public static Usuario isSesionIniciada() {
+
+		Usuario usuario = null;
+		String usuarioId = session("usuarioId");
+
+		if (usuarioId != null) {
+			usuario = Usuario.find.ref(Long.parseLong(usuarioId));
+
+			if (usuario != null) {
+				session("usuarioId", usuarioId);
+			}
+		}
+
+		return usuario;
+
+	}
+
+	public static Result registroNuevo() {
 		return ok(registro.render());
 	}
-	
-	
-	static Form<UsuarioController> usuarioForm = Form.form(UsuarioController.class);
+
+	static Form<UsuarioController> usuarioForm = Form
+			.form(UsuarioController.class);
 
 }
