fe5cd1dd4819c8f52c08d354765515a4e0de749d
BeLi <belipowa@gmail.com>
2013-05-02 18:03:17 +0200

    Arreglado fichero .gitignore

--- app/controllers/UsuarioController.java
+++ app/controllers/UsuarioController.java
@@ -7,43 +7,45 @@ import play.mvc.Result;
 import views.html.usuario.registro;
 
 public class UsuarioController extends Controller {
-	
-	public class Login{
+
+	public class Login {
 		String email;
-		String password;		
+		String password;
 	}
 
-//	public static Usuario isSesionIniciada() {
-//
-//		Usuario usuario = null;
-//		String usuarioId = session("usuarioId");
-//
-//		if (usuarioId != null) {
-//			usuario = Usuario.find.ref(Long.parseLong(usuarioId));
-//
-//			if (usuario != null) {
-//				session("usuarioId", usuarioId);
-//			}
-//		}
-//
-//		return usuario;
-//
-//	}
+	// public static Usuario isSesionIniciada() {
+	//
+	// Usuario usuario = null;
+	// String usuarioId = session("usuarioId");
+	//
+	// if (usuarioId != null) {
+	// usuario = Usuario.find.ref(Long.parseLong(usuarioId));
+	//
+	// if (usuario != null) {
+	// session("usuarioId", usuarioId);
+	// }
+	// }
+	//
+	// return usuario;
+	//
+	// }
 
 	public static Result registroNuevo() {
-		return ok(registro.render());
-	}
-	
-	public static Result iniciarSesion(){
-		
-		//Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
-		
-		return TODO;
-		
-		
+		return ok(registro.render(usuarioForm));
 	}
 
-	static Form<UsuarioController> usuarioForm = Form
-			.form(UsuarioController.class);
+	public static Result iniciarSesion() {
+
+		// Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+
+		return TODO;
+
+	}
+
+	public static Result crearUsuario() {
+		return TODO;
+	}
+
+	static Form<Usuario> usuarioForm = Form.form(Usuario.class);
 
 }
