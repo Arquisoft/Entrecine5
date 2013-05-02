4a3aa7f566e3e63b188ea10afd53ee8ae0064042
Aitor Baragaño Fernández <aitorbf@gmail.com>
2013-05-02 23:28:24 +0200

    Actualización de la parte de administración

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -8,16 +8,24 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.admin.anadir;
 import views.html.admin.index;
-import views.html.admin.sesiones;
 import views.html.admin.anadirSesion;
-import views.html.admin.salas;
 import views.html.admin.anadirSala;
 import views.html.admin.editarPelicula;
+import views.html.admin.fichaPelicula;
 
 public class AdminController extends Controller {
 
 	public static Result index() {
-		return ok(index.render(Pelicula.all(), peliculaForm));
+		return ok(index.render(Pelicula.all(), peliculaForm, Sala.all(),
+				salaForm, Sesion.all(), sesionForm));
+	}
+	
+	public static Result verFichaPelicula(Long id){
+		
+		Pelicula pelicula = Pelicula.find.ref(id);
+
+		return ok(fichaPelicula.render(pelicula));
+		
 	}
 
 	public static Result nuevaPelicula() {
@@ -29,7 +37,8 @@ public class AdminController extends Controller {
 		Form<Pelicula> formularioCumplimentado = peliculaForm.bindFromRequest();
 
 		if (formularioCumplimentado.hasErrors()) {
-			return badRequest(index.render(Pelicula.all(), peliculaForm));
+			return badRequest(index.render(Pelicula.all(), peliculaForm, Sala.all(),
+					salaForm, Sesion.all(), sesionForm));
 		}
 
 		Pelicula.create(formularioCumplimentado.get());
@@ -43,72 +52,62 @@ public class AdminController extends Controller {
 
 		return redirect(routes.ClienteController.index());
 	}
-	
-	public static Result editarPelicula(Long id){
-		
+
+	public static Result editarPelicula(Long id) {
+
 		Pelicula pelicula = Pelicula.find.ref(id);
 
-		return ok(editarPelicula.render(pelicula,peliculaForm));
+		return ok(editarPelicula.render(pelicula, peliculaForm));
 	}
-	
 
-	
-	public static Result verSesiones() {
-		return ok(sesiones.render(Sesion.all(), sesionForm));
-	}
-	
 	public static Result nuevaSesion() {
 		return ok(anadirSesion.render(sesionForm));
 	}
-	
+
 	public static Result crearSesion() {
 
 		Form<Sesion> formularioCumplimentado = sesionForm.bindFromRequest();
 
 		if (formularioCumplimentado.hasErrors()) {
-			return badRequest(sesiones.render(Sesion.all(), sesionForm));
+			return badRequest(index.render(Pelicula.all(), peliculaForm, Sala.all(),
+					salaForm, Sesion.all(), sesionForm));
 		}
 
 		Sesion.create(formularioCumplimentado.get());
-		return redirect(routes.AdminController.verSesiones());
+		return redirect(routes.AdminController.index());
 
 	}
-	
+
 	public static Result eliminarSesion(Long id) {
 
 		Sesion.remove(id);
 
+		return redirect(routes.AdminController.index());
+	}
 
-		return redirect(routes.AdminController.verSesiones());
-	}
-	
-	public static Result verSalas() {
-		return ok(salas.render(Sala.all(), salaForm));
-	}
-	
 	public static Result nuevaSala() {
 		return ok(anadirSala.render(salaForm));
 	}
-	
+
 	public static Result crearSala() {
 
 		Form<Sala> formularioCumplimentado = salaForm.bindFromRequest();
 
 		if (formularioCumplimentado.hasErrors()) {
-			return badRequest(salas.render(Sala.all(), salaForm));
+			return badRequest(index.render(Pelicula.all(), peliculaForm, Sala.all(),
+					salaForm, Sesion.all(), sesionForm));
 		}
 
 		Sala.create(formularioCumplimentado.get());
-		return redirect(routes.AdminController.verSalas());
+		return redirect(routes.AdminController.index());
 
 	}
-	
+
 	public static Result eliminarSala(String id) {
 
 		Sala.remove(id);
-		
 
-		return redirect(routes.AdminController.verSalas());
+		return redirect(routes.AdminController.index());
 	}
 
 	static Form<Pelicula> peliculaForm = Form.form(Pelicula.class);
