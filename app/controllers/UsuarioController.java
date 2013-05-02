a09d8dfc02012336abadea939c8ae6a5067f960e
Aitor Baragaño Fernández <aitorbf@gmail.com>
2013-05-03 00:01:38 +0200

    #

--- app/controllers/UsuarioController.java
+++ app/controllers/UsuarioController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controllers;
 
 import models.Usuario;
@@ -50,56 +49,3 @@ public class UsuarioController extends Controller {
 	static Form<Usuario> usuarioForm = Form.form(Usuario.class);
 
 }
-=======
-package controllers;
-
-import models.Usuario;
-import play.data.Form;
-import play.mvc.Controller;
-import play.mvc.Result;
-import views.html.usuario.registro;
-
-public class UsuarioController extends Controller {
-
-	public class Login {
-		String email;
-		String password;
-	}
-
-	// public static Usuario isSesionIniciada() {
-	//
-	// Usuario usuario = null;
-	// String usuarioId = session("usuarioId");
-	//
-	// if (usuarioId != null) {
-	// usuario = Usuario.find.ref(Long.parseLong(usuarioId));
-	//
-	// if (usuario != null) {
-	// session("usuarioId", usuarioId);
-	// }
-	// }
-	//
-	// return usuario;
-	//
-	// }
-
-	public static Result registroNuevo() {
-		return ok(registro.render(usuarioForm));
-	}
-
-	public static Result iniciarSesion() {
-
-		// Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
-
-		return TODO;
-
-	}
-
-	public static Result crearUsuario() {
-		return TODO;
-	}
-
-	static Form<Usuario> usuarioForm = Form.form(Usuario.class);
-
-}
->>>>>>> fe5cd1dd4819c8f52c08d354765515a4e0de749d
